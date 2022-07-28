e0357587547ed13b493b236177d003b6de7456a8
Stannis <zalevschi1998@gmail.com>
2022-07-29 01:29:42 +0300

    Worked with responsivity

--- src/components/CharacterInfo/CharacterInfo.js
+++ src/components/CharacterInfo/CharacterInfo.js
@@ -1,84 +1,82 @@
-import PropTypes from 'prop-types';
-import Spinner from '../Spinner/Spinner';
-import Button from '../Button/Button';
-import Skeleton from '../Skeleton/Skeleton';
-import { useEffect, useState } from 'react';
-import CharacterImage from '../CharacterImage/CharacterImage';
+import PropTypes from "prop-types";
+import Spinner from "../Spinner/Spinner";
+import Button from "../Button/Button";
+import Skeleton from "../Skeleton/Skeleton";
+import { useEffect, useState } from "react";
+import CharacterImage from "../CharacterImage/CharacterImage";
 
-import './CharacterInfo.scss';
+import "./CharacterInfo.scss";
 
 const CharacterInfo = (props) => {
+  const character = props.character;
 
-    const character = props.character;
-    
-    const [loading, setLoading] = useState(false);
-    
-    useEffect(() => {
-        if (!character) {
-            return;
-        }
+  const [loading, setLoading] = useState(false);
 
-        setLoading(true);
-        setTimeout(() => setLoading(false), 500);
-    }, [character]);
+  useEffect(() => {
+    if (!character) {
+      return;
+    }
 
-    const skeleton = character || loading ? null : <Skeleton/>; 
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || !character) ? <View character={character}/> : null;
+    setLoading(true);
+    setTimeout(() => setLoading(false), 500);
+  }, [character]);
 
+  const skeleton = character || loading ? null : <Skeleton />;
+  const spinner = loading ? <Spinner /> : null;
+  const content = !(loading || !character) ? (
+    <View character={character} />
+  ) : null;
+
+  return (
+    <div>
+      <div className="CharacterInfo">
+        {skeleton}
+        {spinner}
+        {content}
+      </div>
+    </div>
+  );
+};
+
+const View = ({ character }) => {
+  const { thumbnail, name, description, homepage, wiki, comics } = character;
+
+  let comicsList = comics.slice(10);
+  comicsList = comicsList.map((item, index) => {
     return (
-        <div>
-            <div className="CharacterInfo">
-                {skeleton}
-                {spinner}
-                {content}
-            </div>
+      <div className="CharacterInfo-ComicsItem" key={index}>
+        {item.name}
+      </div>
+    );
+  });
+
+  return (
+    <>
+      <div className="CharacterInfo-Header">
+        <CharacterImage thumbnail={thumbnail} name={name} />
+        <div className="CharacterInfo-Details">
+          <div className="CharacterInfo-Name">{name}</div>
+          <div className="CharacterInfo-Btns">
+            <Button label="Homepage" href={homepage} />
+            <Button label="Wiki" isGrey={true} href={wiki} />
+          </div>
         </div>
-    );
-}
-
-const View = ({character}) => {
-    const {thumbnail, name, description, homepage, wiki, comics} = character;
-
-    let comicsList = comics.slice(10);
-    comicsList = comicsList.map((item, index) => {
-        return (<div className="CharacterInfo-ComicsItem" key={index}>
-                    {item.name}
-                </div>);
-    });
-
-    return (
-        <>
-            <div className="CharacterInfo-Header">
-                <CharacterImage 
-                    thumbnail={thumbnail} 
-                    name={name} 
-                />
-                <div className="CharacterInfo-Details">
-                    <div className="CharacterInfo-Name">
-                        {name}
-                    </div>
-                    <div className="CharacterInfo-Btns">
-                        <Button label='Homepage' href={homepage}/>
-                        <Button label='Wiki' isGrey={true} href={wiki}/>
-                    </div>
-                </div>
-            </div>
-            <div className="CharacterInfo-Descr">
-                {!description ? 'There is no description' : null}
-                {description}
-            </div>
-            <div className="CharacterInfo-Comics">Comics: </div>
-            <div className="CharacterInfo-ComicsList">
-                {comicsList.length === 0 ? 'There is no comics.' : null}
-                {comicsList}            
-            </div>
-        </>
-    );
-} 
+      </div>
+      <div className="CharacterInfo-Descr">
+        {!description ? "There is no description" : null}
+        {description}
+      </div>
+      <div className="CharacterInfo-Comics">Comics: </div>
+      <div className="CharacterInfo-ComicsList">
+        {comicsList.length === 0 ? "There is no comics." : null}
+        {comicsList}
+      </div>
+    </>
+  );
+};
 
 CharacterInfo.propTypes = {
-    characterId: PropTypes.object
-}
+  characterId: PropTypes.object,
+};
 
 export default CharacterInfo;
