45adfa5376eaba75281c253a5befc61a0a696caa
Stannis <zalevschi1998@gmail.com>
2022-07-30 12:26:01 +0300

    Worked with responsivity for the comics page

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,12 +5,12 @@ import avengersLogo from "../../resources/img/Avengers_logo.png";
 export default function Banner() {
   return (
     <div className="Banner">
-      <img src={avengers} alt="Avengers" />
+      <img className="Banner-Avengers" src={avengers} alt="Avengers" />
       <div className="Banner-Text">
         New comics every week! <br />
         Stay tuned!
       </div>
-      <img src={avengersLogo} alt="Avengers logo" />
+      <img className="Banner-Logo" src={avengersLogo} alt="Avengers logo" />
     </div>
   );
 }
